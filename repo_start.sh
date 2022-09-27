e1ecf1c214e816f4ffda806706be21f43316520e
flatmonster <114151218+flatmonster@users.noreply.github.com>
2022-09-27 14:47:59 +0900

    add qemu

--- repo_start.sh
+++ repo_start.sh
@@ -12,7 +12,9 @@ repo sync -j4
 # <project path="optee_client"         name="flatmonster/optee_client.git" />
 # <project path="optee_os"             name="flatmonster/optee_os.git" />
 # <project path="optee_examples"       name="flatmonster/optee_examples.git" />
+# <project path="qemu"                 name="qemu/qemu.git"     revision="refs/tags/v7.0.0" clone-depth="1" />
 
 git clone --recursive https://github.com/flatmonster/optee_client.git
 git clone --recursive https://github.com/flatmonster/optee_os.git
 git clone --recursive https://github.com/flatmonster/optee_examples.git
+git clone --recursive https://github.com/flatmonster/qemu.git -b v7.0.0 --depth 1

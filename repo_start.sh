468b8f83dc4816c21d6cebc39fc726b8bc933987
flatmonster <114151218+flatmonster@users.noreply.github.com>
2022-09-30 17:57:43 +0900

    use qemu/qemu v7.0.0

--- repo_start.sh
+++ repo_start.sh
@@ -17,11 +17,13 @@ repo sync -j4
 # <project path="qemu"                 name="qemu/qemu.git"     revision="refs/tags/v7.0.0" clone-depth="1" />
 # <project path="build"                name="OP-TEE/build.git">
 
-git clone --recursive https://github.com/flatmonster/build.git
-git clone --recursive https://github.com/flatmonster/optee_client.git
-git clone --recursive https://github.com/flatmonster/optee_os.git
-git clone --recursive https://github.com/flatmonster/optee_examples.git
-git clone --recursive https://github.com/flatmonster/qemu.git --depth 1
+git clone --recursive https://github.com/flatmonster/build.git &
+git clone --recursive https://github.com/flatmonster/optee_client.git &
+git clone --recursive https://github.com/flatmonster/optee_os.git &
+git clone --recursive https://github.com/flatmonster/optee_examples.git &
+# git clone --depth 1 --recursive https://github.com/flatmonster/qemu.git &
+git clone  --depth 1 --recursive https://github.com/qemu/qemu.git -b v7.0.0 &
 
 cp build/qemu_v8.mk build/Makefile
+cd build
 make -j2 toolchains

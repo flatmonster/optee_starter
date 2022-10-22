2acd421cddb36713c357a8d42c4f65a3e9ea6ab8
flatmonster <114151218+flatmonster@users.noreply.github.com>
2022-10-22 21:39:26 +0900

    wow

--- repo_start.sh
+++ repo_start.sh
@@ -5,7 +5,8 @@
 # 僕の環境のrepoを持ってくる
 mkdir -p work
 cd work
-repo init -u https://github.com/flatmonster/manifest.git -m qemu_v8.xml
+repo init -u https://github.com/flatmonster/manifest.git -m qemu_v8.xml -b optee_3.18.0
+#repo init -u https://github.com/flatmonster/manifest.git -m qemu_v8.xml -b optee_3.18.0-darknetz
 
 # repoに記載された環境を作る
 repo sync -j4
